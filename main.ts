1a316b663f8d70a86897762d2296411d9c6f2b9b
Javier Castro González <alu0100825145@ull.edu.es>
2023-11-09 18:41:15 +0000

    probando a hacer un contador

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
-    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Ghost)
+    for (let index = 0; index <= 10; index++) {
+        basic.showNumber(index)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+    }
 })
+music.play(music.stringPlayable("C5 B A E G C D - ", 120), music.PlaybackMode.UntilDone)
